f58870f7bf4c079455f18b6ca3cb4376646246c3
clarksy <anthony.cl@live.com>
2024-07-20 16:43:39 -0700

    added comments to code

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ filename = "books/frankenstein.txt"
 
 def main():
 
-    with open(filename) as f:
-        file_contents = f.read()
-        words = file_contents.split()
+    with open(filename) as f:           # open file
+        file_contents = f.read()        # read contents
+        words = file_contents.split()   # split string into words
         word_count = len(words)
 
-        for eachword in words:
+        # count characters of each word
+        for eachword in words:        
             count_chars(eachword)
 
+        # sort dictionary by value in descending order
         sortedchars = dict(sorted(chars.items(), key=lambda item: item[1], reverse=True))
 
+        # print results
         print(f"--- begin report of {filename} ---\n")
         print(f"{word_count} words found in the document\n")
         for key, item in sortedchars.items():
             print(f"the \'{key}\' character was found {item} times")
         print("\n--- end report ---")
 
+# count each character and update dictionary count values
 def count_chars(word):
     target = word.lower()
     for char in target:
